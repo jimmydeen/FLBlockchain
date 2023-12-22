ac4b5f475f5d746f32426cac54853b3c0d0484c4
Joong Do Chiang <jchi0099@student.monash.edu>
2023-12-22 18:04:44 +0900

    Add comments for coordinator contract

--- integration/coordinator.sol
+++ integration/coordinator.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Coordinator is Ownable {
 
-    event GlobalModelUpdated(GlobalModel _globalModel);
+    event GlobalModelUpdated(uint32 modelNumber);
 
     struct ModelUpdate {
         string updateHash;
@@ -18,25 +18,29 @@ contract Coordinator is Ownable {
     }
 
     GlobalModel private globalModel;
+    // List of approved trainers that can submit updates/access global model
+    address[] private approvedTrainers;
 
-
-
+    // Mapping of latest model update from each trainer
     mapping(address => ModelUpdate) private modelUpdates;
 
+    // For trainer to submit update
     function submitUpdate(string memory _updatehash, uint256 _numDatapoints) public {
         ModelUpdate memory update = ModelUpdate(_updatehash, _numDatapoints);
         modelUpdates[msg.sender] = update;
 
     }
 
+    // To update global model
     function updateGlobalModel(GlobalModel memory _globalModel) public onlyOwner {
         globalModel = _globalModel;
 
         emit GlobalModelUpdated(_globalModel);
     }
 
+    // To send incentive to trainer
     function distributeIncentive(address payable _trainerAddress, uint256 _incentiveAmount) public onlyOwner {
-        
+
     }
     
 }
