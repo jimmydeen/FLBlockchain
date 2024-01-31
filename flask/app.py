fd96d967c9f34cdc514e5ce4085d215699b3b9c4
Joong Do Chiang <jchi0099@student.monash.edu>
2024-01-31 18:21:37 +1100

    Add deploy_contract endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, make_response
+import sys
+sys.path.append('/Users/jd/Desktop/work/FLBlockchain/integration/')
+from coordinatorcontract import CoordinatorContract
 import subprocess
 import os
+import json
 
 app = Flask(__name__)
 
@@ -48,10 +52,38 @@ def start_server():
 
 @app.route('/start_client', methods=['POST'])
 def start_client():
-    client_id = request.json.get('client_id')
-    other_param = request.json.get('other_param')
-    subprocess.Popen(["python", "flower/flclient.py", client_id, other_param])
+    _client_address = request.json.get('_client_address')
+    _client_pk = request.json.get('_client_pk')
+    _web3_endpoint = request.json.get('_web3_endpoint')
+    _chainid = request.json.get('_chainid')
+    _contract_address = request.json.get('_contract_address')
+    _contract_abi = request.json.get('_contract_abi')
+    server_address = request.json.get('server_address')
+
+    # Start c1.py with the provided parameters
+    subprocess.Popen(['python', 'c1.py', _client_address, _client_pk, _web3_endpoint, str(_chainid), _contract_address, json.dumps(_contract_abi), server_address])
+
     return 'Client started', 200
+
+@app.route('/deploy_contract', methods=['POST'])
+def deploy_contract():
+    contract_path = request.json.get('contract_path')
+    w3provider = request.json.get('w3provider')
+    chain_id = request.json.get('chain_id')
+    sender_address = request.json.get('sender_address')
+    private_key = request.json.get('private_key')
+    incentive = request.json.get('incentive')
+    numberUpdatesRequested = request.json.get('numberUpdatesRequested')
+    maxDataPoints = request.json.get('maxDataPoints')
+    stake = request.json.get('stake')
+
+    # Instantiate the CoordinatorContract class
+    contract = CoordinatorContract(contract_path, w3provider, chain_id, sender_address, private_key)
+
+    # Call the deployContract method
+    contract.deployContract(incentive, numberUpdatesRequested, maxDataPoints, stake)
+
+    return 'Contract deployed', 200
 
 if __name__ == '__main__':
     app.run(port=5000)
