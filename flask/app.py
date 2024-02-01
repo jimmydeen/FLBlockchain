9d11c43c690845aeef6c9a8121552edde44fac39
Joong Do Chiang <jchi0099@student.monash.edu>
2024-02-01 18:56:33 +1100

    Add check_server and modify deploy_contract to return contract address and abi

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 import sys
 sys.path.append('/Users/jd/Desktop/work/FLBlockchain/integration/')
 from coordinatorcontract import CoordinatorContract
@@ -7,6 +7,7 @@ import os
 import json
 
 app = Flask(__name__)
+server_started = False
 
 @app.route('/download_client', methods=['POST'])
 def download_client():
@@ -47,7 +48,10 @@ if __name__ == "__main__":
 
 @app.route('/start_server', methods=['POST'])
 def start_server():
+    global server_started
     subprocess.Popen(["python", "/Users/jd/Desktop/work/FLBlockchain/flower/flserver.py"])
+    
+    server_started = True
     return 'Server started', 200
 
 # Demo Endpoints
@@ -100,7 +104,7 @@ def start_client_3():
 
 @app.route('/deploy_contract', methods=['POST'])
 def deploy_contract():
-    contract_path = request.json.get('contract_path')
+    # contract_path = request.json.get('contract_path')
     w3provider = request.json.get('w3provider')
     chain_id = request.json.get('chain_id')
     sender_address = request.json.get('sender_address')
@@ -111,17 +115,33 @@ def deploy_contract():
     stake = request.json.get('stake')
 
 
+    subprocess.run(["python", "/Users/jd/Desktop/work/FLBlockchain/integration/deploycontract.py", w3provider, str(chain_id), sender_address, private_key, str(incentive), str(numberUpdatesRequested), str(maxDataPoints), str(stake)])
+
+    # Load contract data
+
+    with open("/Users/jd/Desktop/work/FLBlockchain/integration/contract_data.json", "r") as f:
+        data = json.load(f)
+    
+    contract_address = data["address"]
+    contract_abi = data["abi"]
+
+    return jsonify({
+        'contract_address': contract_address,
+        'contract_abi': contract_abi
+    }), 200
 
 
-    # # Instantiate the CoordinatorContract class
-    # contract = CoordinatorContract(contract_path, w3provider, chain_id, sender_address, private_key)
+@app.route('/check_server', methods=['GET'])
+def check_server():
+    if server_started:
+        return jsonify({
+            'message': 'Server started',
+            # 'server_address': server_address,
+            # 'other_param': other_param
+        }), 200
+    else:
+        return 'Server not started', 202
 
-    # # Call the deployContract method
-    # contract.deployContract(incentive, numberUpdatesRequested, maxDataPoints, stake)
-
-
-
-    return 'Contract deployed', 200
 
 if __name__ == '__main__':
     app.run(port=5000)
